aee9a92687e696fcb69260e2dae6e54054c6b8e8
dzivenu <makafuigdzivenu@gmail.com>
2019-10-12 09:16:11 +0000

    fix typos and ad link to analytics

--- src/views/Privacy.vue
+++ src/views/Privacy.vue
@@ -3,8 +3,10 @@
     <div>
       <div class="heading">Privacy</div>
       <div>
-        We use Google Analysitcs on this site, hence the Terms and Privacy policy of Google Analytics apply to this site.
-        Aside this, we do not gather any other type of intelligence.
+        We use <a 
+          href="https://analytics.google.com" 
+          target="_blank">Google Analytics</a> on this site, hence the Terms and Privacy policy of Google Analytics apply to this site.
+          Aside this, we do not gather any other type of intelligence.
       </div>
     </div>
   </div>
