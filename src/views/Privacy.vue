dfa9b748a65516d6ae10ba118ef2f2aa4dae197f
dzivenu <makafuigdzivenu@gmail.com>
2020-01-12 12:52:22 +0000

    --

--- src/views/Privacy.vue
+++ src/views/Privacy.vue
@@ -5,8 +5,12 @@
       <div>
         We use <a 
           href="https://analytics.google.com" 
-          target="_blank">Google Analytics</a> on this site, hence the Terms and Privacy Policy of Google Analytics apply to this site.
-          Aside this, we do not gather any other form of intelligence.
+          target="_blank">Google Analytics</a> on this site, hence the <a 
+            href="https://www.google.com/analytics/terms/?hl=en_US" 
+            target="_blank">Terms</a> and <a 
+              href="https://policies.google.com/privacy" 
+              target="_blank">Privacy Policy</a> of Google Analytics apply to this site.
+              Aside this, we do not gather any other form of intelligence.
       </div>
     </div>
   </div>
